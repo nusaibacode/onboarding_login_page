1f57924e111039580f36e928cce01a4ae64ff177
Nusaiba Rahman <nusaiba@Nusaibas-MacBook-Pro.local>
2022-06-02 15:57:35 -0400

    fetch with async

--- login_page/src/App.js
+++ login_page/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import * as Database from "./constants/database.js";
+import axios from "axios";
 
 import "./styles.css";
 
@@ -8,9 +9,11 @@ function App() {
   // React States
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [loggedInUsername, setLoggedInUsername] = useState({});
+
 
   const userDatabase = Database.userDatabase; 
-  const errors = Database.errors;
+  const errors = Database.errors;  
 
 
   const handleSubmit = (event) => {
@@ -28,13 +31,43 @@ function App() {
         // Invalid password
         setErrorMessages({ name: "pass", message: errors.pass });
       } else {
+        setLoggedInUsername(userData.username);
         setIsSubmitted(true);
       }
     } else {
       // Username not found
       setErrorMessages({ name: "uname", message: errors.uname });
     }
+
   };
+
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+      const loadPost = async () => {
+
+          // Till the data is fetch using API 
+          // the Loading page will show.
+          setLoading(true);
+
+          // Await make wait until that 
+          // promise settles and return its result
+          const response = await axios.get(
+          // "https://jsonplaceholder.typicode.com/posts/");
+          "http://numbersapi.com/random/math");
+
+
+          // After fetching data stored it in posts state.
+          setPosts(response.data);
+
+          // Closed the loading page
+          setLoading(false);
+      }
+
+      // Call the function
+      loadPost();
+  }, []);
 
   // Generate JSX code for error message
   const renderErrorMessage = (name) =>
@@ -63,13 +96,37 @@ function App() {
     </div>
   );
 
+    // const data = (posts.map((item) =>
+    // // Presently we only fetch 
+    // // title from the API 
+    // <h4>{item.title}</h4>))
+
+    const data = (
+    <h4>{posts}</h4>)
+
+
+    // JSX code for User's profile, logged in
+    const userProfile = (
+      <div>
+      <div>Welcome {loggedInUsername}! You're successfully logged in!</div>
+      <div>Here's a fun math fact: </div>
+      <h4>{data}</h4>
+      </div>
+    );
+
   return (
     <div className="app">
       <div className="login-form">
         <div className="title">Sign In</div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+        {isSubmitted ? 
+        // (posts.map((item) =>
+        //                 <h4>User logged in! {item.title}</h4>)
+        //             ) 
+        userProfile
+                    : renderForm}
       </div>
     </div>
+
   );
 }
 
